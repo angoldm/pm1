849a34503adf1d92e268b4c0f0559753370e259b
Dmitriy <angoldm@gmail.com>
2022-03-07 20:50:19 +0500

    Created a Spring boot project with connection to postgresql database

---
+++ src/main/java/ru/angoldm/pm1/Pm1Application.java
@@ -0,0 +1,12 @@
+package ru.angoldm.pm1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pm1Application.class, args);
+	}
+
+}
